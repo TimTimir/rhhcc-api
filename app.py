1f2638b75040ab0e61bdd1882918f73184042817
TimmyTF <tflomin@yandex.ru>
2022-02-16 23:40:24 +0300

    hello world example changed to use tika to parse local file

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from tika import parser
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Sammy!'
+	raw = parser.from_file('sample_rhhcc.pdf', 'https://rhhcc-tika-bdxsr.ondigitalocean.app/tika')
+	return raw['content']
